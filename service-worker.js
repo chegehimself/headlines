001edd525e09101fbc654584c4aebf762bb8d7f6
james-chege <jymeschege@gmail.com>
2018-06-07 02:29:10 +0300

    config caching and  fetching

--- service-worker.js
+++ service-worker.js
@@ -13,26 +13,53 @@ const offlineStuff =  [
 ];
 
 self.addEventListener('install', (e) => {
-	console.log('service worker is installed');
+	console.log('service worker is installed!');
 	e.waitUntil(
 		caches.open(cacheName).then((cache) => {
-			console.log("caching stuffs");
+			console.log("caching stuffs...");
 			return cache.addAll(offlineStuff);
 		})
 		);
 });
 
 self.addEventListener('activate', (e) => {
-    console.log('Activate');
+    console.log('Activating...');
     e.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== cacheName) {
-                    console.log('Keeping only needed cache', key);
+                    console.log('Keeping only needed cache...', key);
                     return caches.delete(key);
                 }
             }));
         })
     );
     return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetching...', e.request.url);
+    // images caching
+    if (e.request.url.endsWith('.jpg') || e.request.url.endsWith('.png')) {
+        e.respondWith(
+            caches.match(e.request).then(response => {
+                if (response) {
+                    console.log('Stuffs in cache:', e.request.url);
+                    return response
+                };
+                fetch(e.request.clone()).then(response => {
+                    if (!response) {
+                        console.log("we found nothing!");
+                        return response;
+                    }
+                    caches.open(cacheName).then(cache => {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                }).catch(err => {
+                    console.log("something very bad happened!");
+                });
+            })
+        );
+    }
 });
