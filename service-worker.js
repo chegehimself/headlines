d32875b8eea3cadaf334340274fa30b6ec8827f4
james-chege <jymeschege@gmail.com>
2018-06-19 21:27:09 +0300

    modify filename and fix caching

--- service-worker.js
+++ service-worker.js
@@ -10,29 +10,16 @@ const offlineStuff =  [
 	// './scripts/search.js',
 	// '/scripts/viewSource.js',
     'https://james-chege.github.io/headlines/index.js',
-    'https://james-chege.github.io/headlines/css/all-skins.min.css',
-    'https://james-chege.github.io/headlines/css/AdminLTE.min.css',
-    'https://james-chege.github.io/headlines/css/normalize.css',
-    'https://james-chege.github.io/headlines/css/custom.css',
-    'https://james-chege.github.io/headlines/scripts/main.js',
-    'https://james-chege.github.io/headlines/scripts/headlines.js',
-    'https://james-chege.github.io/headlines/scripts/search.js',
-    'https://james-chege.github.io/headlines/scripts/sources.js'
-    'https://james-chege.github.io/headlines/scripts/viewSource.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css'
-
-
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/index.html',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/css/_all-skins.min.css',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/css/AdminLTE.min.css',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/css/normalize.css',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/main.js',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/headlines.js',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/search.js',
-  //   'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/viewSource.js',
-  // 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-  // 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css'
+    'https://raw.githubusercontent.com/james-chege/headlines/master/index.html',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/css/all-skins.min.css',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/css/AdminLTE.min.css',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/css/normalize.css',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/main.js',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/headlines.js',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/search.js',
+    'https://raw.githubusercontent.com/james-chege/headlines/master/scripts/viewSource.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install', (event) => {
