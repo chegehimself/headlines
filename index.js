c7354d3240decd2792c2333fcde535dcdbeec3c6
james-chege <jymeschege@gmail.com>
2018-06-19 21:29:48 +0300

    try fix caching

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // register service worker
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
+  navigator.serviceWorker.register('https://james-chege.github.io/headlines/index.js');
 }
